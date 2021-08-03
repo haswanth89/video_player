a1823f5f7e85dca90bf73f5b97173e3f3c38ac85
haswanth89 <haswanth89@gmail.com>
2021-08-03 14:34:39 +0530

    New

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
 
 void main() => runApp(VideoApp());
 
@@ -14,12 +16,23 @@ class _VideoAppState extends State<VideoApp> {
   @override
   void initState() {
     super.initState();
+    
     _controller = VideoPlayerController.network(
         'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
+      accelerometerEvents.listen((AccelerometerEvent event) {
+      if(event.x < -3){
+         _controller.play();}
+      else 
+      {
+        _controller.pause();
+      }
+});
+
+   
   }
 
   @override
